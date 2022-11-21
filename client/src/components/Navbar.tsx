f14a49a1ff8229e4215324f1696507d1631689fd
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-11-21 20:49:37 +0100

    change padding to fit css

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-center bg-black p-5 text-skyOrange">
+    <nav className="flex justify-center bg-black p-7 text-skyOrange">
         <ul className="hover:underline hover:text-white">
         <li><Link to="/">HOME</Link></li>
         </ul>
