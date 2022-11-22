97413064a20f7ccfdee4a809880b2fefe3fb0066
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-11-22 23:20:40 +0100

    Add another css color + extend maxHeight prop with 60vh

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,8 +8,12 @@ module.exports = {
     extend: {
       colors: {
         'skyGrey': '#f4ddc272',
+        'skyBrown': '#d4aa70',
         'skyOrange': 'rgb(255, 166, 0)',
         'skyGreen': '#1ad421'
+      },
+      maxHeight: {
+        '60': '60vh',
       },
     },
   },
